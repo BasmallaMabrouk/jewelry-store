59a11d5a82f6ccbd91effd53bf2ba748c7c9278a
ii20aya <dann742003@gmail.com>
2026-01-23 16:38:07 +0200

    login

---
+++ js/navbar-user.js
@@ -0,0 +1,25 @@
+import { auth, db } from "./CDNFirebase.js";
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { doc, getDoc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+const userBox = document.getElementById("userBox");
+
+onAuthStateChanged(auth, async (user) => {
+    if (user) {
+        const userDoc = await getDoc(doc(db, "users", user.uid));
+
+        if (userDoc.exists()) {
+            const data = userDoc.data();
+            const name = data.displayName;
+            const role = data.role;
+
+            userBox.innerHTML = `
+                <span class="user-greet">
+                    Hi, <b>${role === "admin" ? "Admin" : "User"}</b> ${name}
+                </span>
+            `;
+        }
+    } else {
+        userBox.innerHTML = `<a href="Login.html" id="loginBtn">Login</a>`;
+    }
+});
